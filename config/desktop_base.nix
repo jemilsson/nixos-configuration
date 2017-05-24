c4118a93afd8fe65fc6b59c917a5dc680f24442b
Jonas Emilsson <jonas.emilsson@gmail.com>
2017-05-24 21:51:31 +0200

    udev

--- config/desktop_base.nix
+++ config/desktop_base.nix
@@ -156,15 +156,15 @@
        font-awesome-ttf
      ];
    };
+   services = {
+     udev.extraRules = ''
+        # this udev file should be used with udev 188 and newer
+        ACTION!="add|change", GOTO="u2f_end"
 
-   udev.extraRules = ''
-      # this udev file should be used with udev 188 and newer
-      ACTION!="add|change", GOTO="u2f_end"
+        # Yubico YubiKey
+        KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="1050", ATTRS{idProduct}=="0113|0114|0115|0116|0120|0402|0403|0406|0407|0410", MODE="0660", GROUP="plugdev", TAG+="uaccess"
 
-      # Yubico YubiKey
-      KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="1050", ATTRS{idProduct}=="0113|0114|0115|0116|0120|0402|0403|0406|0407|0410", MODE="0660", GROUP="plugdev", TAG+="uaccess"
-
-      LABEL="u2f_end"
-    '';
-
+        LABEL="u2f_end"
+      '';
+    };
 }
