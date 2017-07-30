e2ae9afedcc64af118ea2e866e75fb65e74686b5
Jonas Emilsson <jonas.emilsson@gmail.com>
2017-07-30 14:58:54 +0200

    https

--- machines/mannie.nix
+++ machines/mannie.nix
@@ -10,7 +10,7 @@ ix{ config, lib, pkgs, ... }:
 
     firewall = {
       enable = true;
-      allowedTCPPorts = [ 22 80 443];
+      allowedTCPPorts = [ 22 80 443 ];
       allowedUDPPorts = [ ];
     };
 
