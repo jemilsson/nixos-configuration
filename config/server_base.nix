f5141861cf8ab73cababadb370027d45148b5233
Jonas Emilsson <jonas.emilsson@dgc.se>
2017-07-31 18:08:21 +0200

    removed fail2ban

--- config/server_base.nix
+++ config/server_base.nix
@@ -15,38 +15,4 @@
       };
 
   };
-
-  services.fail2ban = {
-    enable = true;
-    jails = {
-      DEFAULT =
-        ''
-          bantime  = 3600
-        '';
-      sshd =
-        ''
-          filter = sshd
-          maxretry = 4
-          action   = iptables[name=ssh, port=ssh, protocol=tcp]
-          enabled  = true
-        '';
-
-      sshd-ddos =
-        ''
-          filter = sshd-ddos
-          maxretry = 2
-          action   = iptables[name=ssh, port=ssh, protocol=tcp]
-          enabled  = true
-        '';
-
-      port-scan =
-        ''
-          filter   = port-scan
-          action   = iptables-allports[name=port-scan]
-          maxretry = 2
-          bantime  = 7200
-          enabled  = true
-        '';
-    };
-  };
 }
