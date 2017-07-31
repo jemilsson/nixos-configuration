f9bcd976fddc6e8f744d82a00e4eefb2efdeb791
Jonas Emilsson <jonas.emilsson@dgc.se>
2017-07-31 17:03:24 +0200

    hsts

--- machines/mannie.nix
+++ machines/mannie.nix
@@ -76,7 +76,7 @@
       recommendedProxySettings=true;
       recommendedTlsSettings = true;
       recommendedGzipSettings = true;
-      appendHttpConfig="server_names_hash_bucket_size 128;";
+      appendHttpConfig="server_names_hash_bucket_size 128; add_header Strict-Transport-Security "max-age=31536000; includeSubDomains; preload" always;";
       sslDhparam = "/var/lib/dhparams/nginx.pem";
       virtualHosts = {
         "grafana.jonasem.com" = {
