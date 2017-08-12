f451a3452b33ea212810ba0567a5a190c4187103
Jonas Emilsson <jonas.emilsson@gmail.com>
2017-08-12 18:02:58 +0200

    moved synapse to its own file

--- machines/mannie/configuration.nix
+++ machines/mannie/configuration.nix
@@ -8,6 +8,7 @@ in
     ../../config/services/nginx/nginx.nix
 
     services/gogs.nix
+    services/synapse.nix
   ];
 
   networking = {
@@ -76,39 +77,7 @@ in
     };
    };
 
-   matrix-synapse = {
-     enable = true;
-     enable_registration = false;
-     no_tls = true;
-     web_client = true;
-     public_baseurl = "https://emilsson.chat/";
-     server_name = "emilsson.chat";
-     database_type = "psycopg2";
-     database_args = {
-        user = "synapse";
-        password = "mBgLGniz7i0maQDR";
-        database = "synapse";
-        host = "127.0.0.1";
-        cp_min = "5";
-        cp_max = "10";
-     };
-     listeners = [
-      {
-        bind_address = "127.0.0.1";
-        port = 8448;
-        tls = false;
-        type = "http";
-        x_forwarded = true;
-        resources = [
-          { compress = false;
-            names = [ "client" "webclient"];
-          }
-        ];
-      }
-     ];
 
-
-   };
 
    nginx.virtualHosts = {
 
@@ -139,17 +108,6 @@ in
             root = "/var/www/default";
             index = "index.html";
           };
-          };
-        };
-
-        "emilsson.chat" = {
-          enableSSL = true;
-          forceSSL = true;
-          enableACME = true;
-          locations = {
-            "/" = {
-              proxyPass = "http://localhost:8448";
-            };
           };
         };
 
