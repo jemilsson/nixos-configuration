a1625ad4c8e515fa7f431582e1b713a54eaedca5
Jonas Emilsson <jonas.emilsson@gmail.com>
2017-08-19 18:23:59 +0200

    added form

--- machines/mannie/services/helloflask/helloflask/helloflask/wsgi.py
+++ machines/mannie/services/helloflask/helloflask/helloflask/wsgi.py
@@ -4,6 +4,7 @@ application = Flask(__name__)
 @application.route("/", methods = ['POST', 'GET'])
 def hello():
     print(request.headers)
+    print(request.form)
     return "Hello Flask!"
 
 if __name__ == "__main__":
