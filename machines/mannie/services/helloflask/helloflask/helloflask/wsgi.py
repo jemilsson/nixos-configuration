c3f45558582d84ace55bd157b105d5ac51e79dc5
Jonas Emilsson <jonas.emilsson@gmail.com>
2017-08-19 18:27:24 +0200

    prettyprint

--- machines/mannie/services/helloflask/helloflask/helloflask/wsgi.py
+++ machines/mannie/services/helloflask/helloflask/helloflask/wsgi.py
@@ -1,10 +1,12 @@
 from flask import Flask, request
 application = Flask(__name__)
 
+from pprint import pprint
+
 @application.route("/", methods = ['POST', 'GET'])
 def hello():
-    print(request.headers)
-    print(request.form)
+    pprint(request.headers)
+    pprint(request.form)
     return "Hello Flask!"
 
 if __name__ == "__main__":
