d84847cbec627b4399aab07ee0ec7ffb6fcdf760
Jonas Emilsson <jonas.emilsson@gmail.com>
2017-09-04 20:34:10 +0200

    added statusPage to nginx

--- config/services/nginx/nginx.nix
+++ config/services/nginx/nginx.nix
@@ -9,6 +9,7 @@
        sslCiphers = "AES256+EECDH:AES256+EDH:!aNULL";
        appendHttpConfig="server_names_hash_bucket_size 128; add_header Strict-Transport-Security \"max-age=31536000; includeSubDomains; preload\" always;";
        sslDhparam = "/var/lib/dhparams/nginx.pem";
+       statusPage = true;
     };
     security = {
        dhparams = {
