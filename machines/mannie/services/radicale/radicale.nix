b752615647fdcd9d66c345e75ab65fcd28f8776d
Jonas Emilsson <jonas.emilsson@gmail.com>
2017-09-08 10:30:39 +0200

    radicale

--- machines/mannie/services/radicale/radicale.nix
+++ machines/mannie/services/radicale/radicale.nix
@@ -5,6 +5,23 @@
       enable = true;
       config = pkgs.lib.readFile ./radicale.cfg;
     };
+
+    nginx.virtualHosts = {
+      "cal.jonasem.com" = {
+        enableSSL = true;
+        forceSSL = true;
+        enableACME = true;
+        locations = {
+          "/" = {
+            proxyPass = "http://localhost:3000";
+            extraConfig = ''
+               auth_basic "Restricted";
+               auth_basic_user_file /var/db/htpasswd/users;
+               '';
+          };
+        };
+      };
+    };
 };
 
 nixpkgs.overlays = [
