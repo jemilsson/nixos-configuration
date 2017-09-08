6a7b9c915832a520878719e202b964412773de77
Jonas Emilsson <jonas.emilsson@gmail.com>
2017-09-08 12:32:14 +0200

    moved radicale to root

--- machines/mannie/services/radicale/radicale.nix
+++ machines/mannie/services/radicale/radicale.nix
@@ -12,10 +12,9 @@
         forceSSL = true;
         enableACME = true;
         locations = {
-          "/radicale/" = {
+          "/" = {
             proxyPass = "http://localhost:5232/";
             extraConfig = ''
-               proxy_set_header  X-Script-Name /radicale/;
                proxy_set_header     X-Forwarded-For $proxy_add_x_forwarded_for;
                proxy_set_header     X-Remote-User $remote_user;
                auth_basic "Restricted";
