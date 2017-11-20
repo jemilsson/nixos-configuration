e28b965546f760af69ad4ebfb7bd8dc5cb7223c8
Jonas Emilsson <jonas.emilsson@gmail.com>
2017-11-20 19:58:10 +0100

    powerlevel9k

--- config/base.nix
+++ config/base.nix
@@ -49,7 +49,7 @@ programs = {
     syntaxHighlighting = {
       enable = true;
     };
-    promptInit = "source ${pkgs.unstable.zsh-powerlevel9k}/share/zsh-powerlevel9k/powerlevel9k.zsh-theme";
+    promptInit = "source ${pkgs.zsh-powerlevel9k}/share/zsh-powerlevel9k/powerlevel9k.zsh-theme";
   };
 };
 
