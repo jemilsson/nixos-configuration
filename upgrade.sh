a72d21a061203edd55e34e54aabbf5fe58964c25
Jonas Emilsson <jonas.emilsson@gmail.com>
2018-04-14 18:03:02 +0200

    switched to 18.03

--- upgrade.sh
+++ upgrade.sh
@@ -6,6 +6,6 @@ sudo git clone https://github.com/jemilsson/nixos-configuration.git
 sudo ln -sr /etc/nixos/nixos-configuration/machines/$HOSTNAME/configuration.nix configuration.nix
 sudo nix-channel --add https://nixos.org/channels/nixos-unstable
 nixos-unstable
-sudo nix-channel --add https://nixos.org/channels/nixos-17.09 nixos
+sudo nix-channel --add https://nixos.org/channels/nixos-18.03 nixos
 sudo nix-channel --update
 sudo nixos-rebuild switch --upgrade
