7ef7a7e65bc9a928ff5f59e6003b65eeafae79f7
Jonas Emilsson <jonas.emilsson@gmail.com>
2018-05-13 00:33:37 +0200

    interface

--- machines/brody/configuration.nix
+++ machines/brody/configuration.nix
@@ -16,6 +16,13 @@
     };
 
     interfaces = {
+      "lan@enp0s20f0" = {
+        ipv4 = {
+          addresses = [
+            { address = "10.0.0.1"; prefixLength = 24;}
+          ];
+        };
+      };
 
     };
     vlans = {
