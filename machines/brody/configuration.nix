7ba795631d50524be817aabfdc6590ec57e4d0d4
Jonas Emilsson <jonas.emilsson@gmail.com>
2018-05-13 11:55:20 +0200

    netmask

--- machines/brody/configuration.nix
+++ machines/brody/configuration.nix
@@ -74,7 +74,7 @@
       enable = true;
       interfaces = [ "lan-1" "lan-2" ];
       extraConfig = ''
-        option subnet-mask 255.255.255.0;
+
         option domain-name-servers 1.1.1.1;
         option domain-name "jonas.systems";
 
@@ -82,12 +82,14 @@
           range 10.0.0.100 10.0.0.200;
           option broadcast-address 10.0.0.255;
           option routers 10.0.0.1;
+          option subnet-mask 255.255.255.0;
         }
 
         subnet 10.0.1.0 netmask 255.255.255.0 {
           range 10.0.1.100 10.0.1.200;
           option broadcast-address 10.0.1.255;
           option routers 10.0.1.1;
+          option subnet-mask 255.255.255.0;
         }
 
 
