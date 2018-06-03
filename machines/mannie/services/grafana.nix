305b40afdb1ac119ebcbbe8a68849b9b6932adcf
Jonas Emilsson <jonas.emilsson@gmail.com>
2018-06-03 22:11:38 +0200

    grafana

--- machines/mannie/services/grafana.nix
+++ machines/mannie/services/grafana.nix
@@ -6,7 +6,7 @@
      enable = true;
      addr = "127.0.0.1";
      #package = pkgs.unstable.grafana;
-     package = pkgs.grafana;
+     #package = pkgs.grafana;
 
      database = {
        host = "127.0.0.1:5432";
@@ -36,7 +36,7 @@
 
 nixpkgs.overlays = [
     (self: super: {
-      #grafana = pkgs.unstable.grafana;
+      grafana = pkgs.unstable.grafana;
       #grafana = pkgs.grafana;
     }
     )
