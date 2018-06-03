20b3f6fb9cf25b23520dd2bafef8d9b76fa1aa20
Jonas Emilsson <jonas.emilsson@gmail.com>
2018-06-03 21:24:43 +0200

    fixed linebreak

--- upgrade.sh
+++ upgrade.sh
@@ -4,8 +4,7 @@ sudo rm /etc/nixos/configuration.nix
 cd /etc/nixos
 sudo git clone https://github.com/jemilsson/nixos-configuration.git
 sudo ln -sr /etc/nixos/nixos-configuration/machines/$HOSTNAME/configuration.nix configuration.nix
-sudo nix-channel --add https://nixos.org/channels/nixos-unstable
-nixos-unstable
+sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos-unstable
 sudo nix-channel --add https://nixos.org/channels/nixos-18.03 nixos
 sudo nix-channel --update
 sudo nixos-rebuild switch --upgrade
