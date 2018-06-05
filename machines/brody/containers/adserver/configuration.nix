7d8e0128c4e0351448a033e02d7b99db19314ef1
Jonas Emilsson <jonas.emilsson@gmail.com>
2018-06-05 18:13:22 +0200

    adserver

---
+++ machines/brody/containers/adserver/configuration.nix
@@ -0,0 +1,32 @@
+{ config, pkgs, ... }:
+{
+  imports = [
+    ../../../../config/minimum.nix
+];
+
+networking = {
+  firewall = {
+    enable = false;
+  };
+
+  defaultGateway = {
+    address = "10.0.0.1";
+    interface = "eth0";
+  };
+};
+
+environment.systemPackages = with pkgs; [
+
+];
+
+services.nginx ={
+   enable = true;
+
+   virtualHosts."adserver" = {
+     default = true;
+   };
+
+ };
+
+
+}
---
+++ machines/brody/containers/adserver/container.nix
@@ -0,0 +1,11 @@
+{ config, pkgs, ... }:
+{
+  config = import ./configuration.nix { pkgs = pkgs; config=config; };
+  hostBridge = "br0";
+  localAddress = "10.0.0.8/24";
+  autoStart = true;
+  privateNetwork = true;
+  #forwardPorts = [
+    #{ containerPort = 8123; hostPort = 80; protocol = "tcp"; }
+  #];
+}
