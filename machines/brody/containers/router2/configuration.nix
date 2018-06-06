36d3619e55cdd0c9a05550e780c3afa5cf6514f4
Jonas Emilsson <jonas.emilsson@gmail.com>
2018-06-06 09:55:10 +0200

    dns proxy

--- machines/brody/containers/router2/configuration.nix
+++ machines/brody/containers/router2/configuration.nix
@@ -17,7 +17,11 @@ networking = {
     allowedTCPPorts = [ ];
     allowedUDPPorts = [ ];
 
-    
+    extraCommands = ''
+    iptables -t nat -A nixos-nat-pre -i eth0 -p udp --dport 53 ! -d 10.0.0.5 -j MARK --set-xmark 0x1/0xffffffff
+    iptables -t nat -A nixos-nat-pre -i eth0 -p udp --dport 53 ! -d 10.0.0.5 -j DNAT --to 10.0.0.5
+    iptables -t nat -A nixos-nat-post -o eth0 -p udp --dport 53 -m mark --mark 0x1 -j MASQUERADE
+    '';
   };
 
   nat = {
