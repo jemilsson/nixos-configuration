8455577742b7f96fee54d0536dabe841f79c8c7e
Jonas Emilsson <jonas.emilsson@gmail.com>
2018-06-07 23:00:02 +0200

    routing

--- machines/brody/containers/router1/configuration.nix
+++ machines/brody/containers/router1/configuration.nix
@@ -35,7 +35,9 @@ networking = {
     iptables -I FORWARD -i eth1000-2 -o eth0 -s 10.5.0.0/24 -d 10.0.0.0/24 -j ACCEPT
     iptables -I FORWARD -i eth1000-2 -o eth0 -s 10.5.6.0/24 -d 10.0.0.0/24 -j ACCEPT
     iptables -I FORWARD -i eth1000-2 -o eth0 -s 10.5.6.0/24 -d 10.5.1.0/24 -j ACCEPT
-    iptables -I FORWARD -i eth1000-2 -o eth0 -s 10.5.6.0/24 -d 10.5.2.0/24 -j ACCEPT
+
+    iptables -I FORWARD -i eth1000-2 -o eth1002-2 -s 10.5.6.0/24 -d 10.5.2.0/24 -j ACCEPT
+
 
     '';
   };
