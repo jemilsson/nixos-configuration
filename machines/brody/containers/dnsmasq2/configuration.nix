01e79e72a46e5b14a78a1f291ef3556bd57f5919
Jonas Emilsson <jonas.emilsson@gmail.com>
2018-06-13 21:45:27 +0200

    mask

--- machines/brody/containers/dnsmasq2/configuration.nix
+++ machines/brody/containers/dnsmasq2/configuration.nix
@@ -44,23 +44,23 @@ services.dnsmasq = {
 
     dhcp-lease-max=50
 
-    dhcp-range=oldlan,10.0.0.100,10.0.0.200
+    dhcp-range=oldlan,10.0.0.100,10.0.0.200,255.255.255.0
     dhcp-option=oldlan,3,10.0.0.1
     dhcp-option=oldlan,6,${dnsServerAddress}
 
-    dhcp-range=lan,10.5.1.100,10.5.1.200
+    dhcp-range=lan,10.5.1.100,10.5.1.200,255.255.255.0
     dhcp-option=lan,3,10.5.1.1
     dhcp-option=lan,6,${dnsServerAddress}
 
-    dhcp-range=wlan,10.5.2.100,10.5.2.200
+    dhcp-range=wlan,10.5.2.100,10.5.2.200,255.255.255.0
     dhcp-option=wlan,3,10.5.2.1
     dhcp-option=wlan,6,${dnsServerAddress}
 
-    dhcp-range=guestlan,10.5.3.100,10.5.3.200
+    dhcp-range=guestlan,10.5.3.100,10.5.3.200,255.255.255.0
     dhcp-option=guestlan,3,10.5.3.1
     dhcp-option=guestlan,6,${dnsServerAddress}
 
-    dhcp-range=medialan,10.5.4.100,10.5.4.200
+    dhcp-range=medialan,10.5.4.100,10.5.4.200,255.255.255.0
     dhcp-option=medialan,3,10.5.4.1
     dhcp-option=medialan,6,${dnsServerAddress}
 
