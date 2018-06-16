687b891cdf6b3a00b76f858b05f9e846b8f09a25
Jonas Emilsson <jonas.emilsson@gmail.com>
2018-06-16 19:24:50 +0200

    deCONZ

--- machines/brody/containers/deconz/deconz/default.nix
+++ machines/brody/containers/deconz/deconz/default.nix
@@ -3,7 +3,7 @@
 
 stdenv.mkDerivation rec {
   name = "deconz-${version}";
-  version = "2.05.29";
+  version = "2.05.30";
 
   src = fetchurl {
     url = "https://www.dresden-elektronik.de/deconz/ubuntu/beta/${name}-qt5.deb";
@@ -16,10 +16,15 @@ stdenv.mkDerivation rec {
   dontBuild = true;
 
   unpackPhase = "dpkg-deb -x $src .";
-  installPhase = "
+  installPhase = ''
     mv usr/bin .
     cp -r . $out
-  ";
+
+    ${patchelf}/bin/patchelf \
+      --interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
+      --set-rpath "${lib.makeLibraryPath [  ]}" \
+      $out/bin/deCONZ
+  '';
 
   meta = with stdenv.lib; {
   };
