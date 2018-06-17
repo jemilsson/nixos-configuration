68e0f1eff98fc904919046c31c1089021df5e568
Jonas Emilsson <jonas.emilsson@gmail.com>
2018-06-17 21:25:47 +0200

    pydeconz

--- packages/pydeconz/default.nix
+++ packages/pydeconz/default.nix
@@ -1,4 +1,4 @@
-{ python36, python3Packages  }:
+{ python36, python36Packages  }:
 
 #with import <nixpkgs> {};
 
@@ -13,6 +13,6 @@ python36.pkgs.buildPythonPackage rec {
     sha256 = "1gq13z54k9w4r6nygm2hsfl4yj0gl27hndnlxj0h91g90m2ggs14";
   };
 
-  propagatedBuildInputs = [ python3Packages.aiohttp ];
+  propagatedBuildInputs = [ python36Packages.aiohttp ];
   doCheck = false;
 }
