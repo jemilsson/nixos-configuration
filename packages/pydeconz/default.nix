2716bc72d694bfbd0b963e67428214b2e1fb841c
Jonas Emilsson <jonas.emilsson@gmail.com>
2018-06-17 21:11:42 +0200

    pydeconz

---
+++ packages/pydeconz/default.nix
@@ -0,0 +1,18 @@
+{ pkgs }:
+
+#with import <nixpkgs> {};
+
+python36.pkgs.buildPythonPackage rec {
+  pname = "pydeconz";
+  version = "38";
+
+  disabled = !python36.pkgs.isPy3k;
+
+  src = python36.pkgs.fetchPypi {
+    inherit pname version;
+    sha256 = "1gq13z54k9w4r6nygm2hsfl4yj0gl27hndnlxj0h91g90m2ggs14";
+  };
+
+  buildInputs = [ python36Packages.aiohttp ];
+  doCheck = false;
+}
