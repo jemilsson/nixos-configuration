9be40bc776ca825ba52e8aa48030638db5e9ed68
Jonas Emilsson <jonas.emilsson@gmail.com>
2018-06-17 16:45:16 +0200

    deCONZ

--- machines/brody/containers/deconz/container.nix
+++ machines/brody/containers/deconz/container.nix
@@ -8,7 +8,7 @@
   #additionalCapabilities = ["CAP_NET_BIND_SERVICE" "CAP_KILL" "CAP_SYS_BOOT" "CAP_SYS_TIME"];
   additionalCapabilities = ["all"];
   allowedDevices = [
-    { modifier = "rwm"; node = "/dev/ttyUSB0"; }
+    { modifier = "rwm"; node = "char-usb_device"; }
   ];
   bindMounts = {
     "/dev/ttyUSB0" = {hostPath = "/dev/ttyUSB0"; isReadOnly = false;};
