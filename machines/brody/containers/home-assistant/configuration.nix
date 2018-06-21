31c79445b29b705b368c29ad8d057d5cc565387c
Jonas Emilsson <jonas.emilsson@gmail.com>
2018-06-21 23:36:31 +0200

    longlat

--- machines/brody/containers/home-assistant/configuration.nix
+++ machines/brody/containers/home-assistant/configuration.nix
@@ -36,8 +36,8 @@ services.home-assistant ={
       homeassistant = {
         name = "Home";
         time_zone = "UTC";
-        latitude = 59.35;
-        longitude = 18.05;
+        latitude = "59.35";
+        longitude = "18.05";
         unit_system = "metric";
 
       };
