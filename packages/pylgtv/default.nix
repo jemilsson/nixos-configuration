24fff06eea8f7733e8152fa3ee12117eef6c57d8
Jonas Emilsson <jonas.emilsson@gmail.com>
2018-06-21 23:56:58 +0200

    pylgtv

--- packages/pylgtv/default.nix
+++ packages/pylgtv/default.nix
@@ -13,6 +13,6 @@ python36.pkgs.buildPythonPackage rec {
     sha256 = "1k457x07xkbzibny1rpfq19wnk56frs0jbr3r2q7saxi94hqi05y";
   };
 
-  buildInputs = [ python36Packages.aiohttp python36Packages.asyncio ];
+  buildInputs = [ python36Packages.aiohttp python36Packages.asyncio python36Packages.websockets ];
   doCheck = false;
 }
