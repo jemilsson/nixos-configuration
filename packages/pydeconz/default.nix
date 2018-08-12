6f3ed3d05ed9facd11166cf6979a1a59608526ce
Jonas Emilsson <jonas.emilsson@gmail.com>
2018-08-12 15:34:00 +0200

    upgrade pydeconz

--- packages/pydeconz/default.nix
+++ packages/pydeconz/default.nix
@@ -4,13 +4,13 @@
 
 python36.pkgs.buildPythonPackage rec {
   pname = "pydeconz";
-  version = "38";
+  version = "43";
 
   disabled = !python36.pkgs.isPy3k;
 
   src = python36.pkgs.fetchPypi {
     inherit pname version;
-    sha256 = "1gq13z54k9w4r6nygm2hsfl4yj0gl27hndnlxj0h91g90m2ggs14";
+    sha256 = "5bcccceb51a9e8a09d1c5d76e700b6660283876546e164b9fa579dd776ca19de";
   };
 
   buildInputs = [ python36Packages.aiohttp ];
