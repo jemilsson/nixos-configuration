87daa4dde1c612a4c947f6339c88e62bb620310c
Jonas Emilsson <jonas.emilsson@gmail.com>
2018-10-06 20:15:09 +0200

    zsh autosuggestions

--- config/minimum.nix
+++ config/minimum.nix
@@ -4,7 +4,9 @@
 programs = {
   zsh = {
     enable = true;
-    enableAutosuggestions = true;
+    autosuggestions = {
+      enable = true;
+    };
     syntaxHighlighting = {
       enable = true;
     };
