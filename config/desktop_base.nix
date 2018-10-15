5e5aa6dbe4ec35ad1b8387cbd6b82b60c3fe0c30
Jonas Emilsson <jonas.emilsson@gmail.com>
2018-10-15 22:14:59 +0200

    steam

--- config/desktop_base.nix
+++ config/desktop_base.nix
@@ -89,7 +89,7 @@ in
     #skype
 
     #Games
-    (steam.override { extraPkgs = pkgs: [ glibc xorg.libxcb bash ]; })
+    (steam.override {  extraPkgs = pkgs: with pkgs.pkgsi686Linux; [ glibc xorg.libxcb bash ]; })
 
 
     #Graphical network tools
