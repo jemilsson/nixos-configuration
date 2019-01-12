57571229b799d413177242365f9cae5bc31bf3d3
Jonas Emilsson <jonas.emilsson@gmail.com>
2019-01-12 10:01:36 +0100

    pydeconz hash

--- packages/pydeconz/default.nix
+++ packages/pydeconz/default.nix
@@ -8,7 +8,7 @@ python37.pkgs.buildPythonPackage rec {
 
   src = python37.pkgs.fetchPypi {
     inherit pname version;
-    sha256 = "03qwba8qn3939jwm79jmc8s8lpirdbwdl6chah79544l0lh0vvbf";
+    sha256 = "1d8drgy712sxxvisdaz53jgyiiidbhvnbrsvgci6jh9b6s6189wb";
   };
 
   buildInputs = [ python37Packages.aiohttp ];
