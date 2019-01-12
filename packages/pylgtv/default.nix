ba0df6df04f76abdeb8a3a7ab0f17958c91e5202
Jonas Emilsson <jonas.emilsson@gmail.com>
2019-01-12 10:23:15 +0100

    deps

--- packages/pylgtv/default.nix
+++ packages/pylgtv/default.nix
@@ -13,6 +13,6 @@ python37.pkgs.buildPythonPackage rec {
     sha256 = "0k2cj33mnfp914kvj698ldxw2807f6z1l1jr1h99h1xfdwrkz80f";
   };
 
-  buildInputs = [ python37Packages.aiohttp python37Packages.asyncio python37Packages.websockets ];
+  buildInputs = [ python37Packages.aiohttp python37Packages.asyncio python37Packages.websockets python37Packages.coverage ];
   doCheck = false;
 }
