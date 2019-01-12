8d9a0c17fa487dcb42e11baa1b32cd1bca838b85
Jonas Emilsson <jonas.emilsson@gmail.com>
2019-01-12 19:51:48 +0100

    deps

--- packages/pydeconz/default.nix
+++ packages/pydeconz/default.nix
@@ -1,4 +1,4 @@
-{ python37, python37Packages  }:
+{ python37, python37Packages, pkgs  }:
 
 #with import <nixpkgs> {};
 
@@ -11,6 +11,6 @@ python37.pkgs.buildPythonPackage rec {
     sha256 = "1d8drgy712sxxvisdaz53jgyiiidbhvnbrsvgci6jh9b6s6189wb";
   };
 
-  #buildInputs = [ python37Packages.aiohttp python37Packages.typing ];
+  buildInputs = [ pkgs.unstable.python37Packages.aiohttp ];
   doCheck = false;
 }
