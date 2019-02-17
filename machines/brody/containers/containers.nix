c19f8dced85d2b6a0f21bc28a001e67bc5a3de7b
Jonas Emilsson <jonas.emilsson@gmail.com>
2019-02-17 17:15:29 +0100

    disabling some legacy services

--- machines/brody/containers/containers.nix
+++ machines/brody/containers/containers.nix
@@ -1,14 +1,14 @@
 { config, pkgs, stdenv, ... }:
 {
-  "router1" = import ./router1/container.nix { pkgs = pkgs; config=config; };
-  "router2" = import ./router2/container.nix { pkgs = pkgs; config=config; };
+  #"router1" = import ./router1/container.nix { pkgs = pkgs; config=config; };
+  #"router2" = import ./router2/container.nix { pkgs = pkgs; config=config; };
   "router4" = import ./router4/container.nix { pkgs = pkgs; config=config; };
   #"dnsmasq" = import ./dnsmasq/container.nix { pkgs = pkgs; config=config; };
-  "stubby" = import ./stubby/container.nix { pkgs = pkgs; config=config; };
+  #"stubby" = import ./stubby/container.nix { pkgs = pkgs; config=config; };
   "home-assistant" = import ./home-assistant/container.nix { pkgs = pkgs; config=config; };
-  "adserver" = import ./adserver/container.nix { pkgs = pkgs; config=config; };
+  #"adserver" = import ./adserver/container.nix { pkgs = pkgs; config=config; };
   #"dhcp" = import ./dhcp/container.nix { pkgs = pkgs; config=config; };
-  "dnsmasq2" = import ./dnsmasq2/container.nix { pkgs = pkgs; config=config; };
+  #"dnsmasq2" = import ./dnsmasq2/container.nix { pkgs = pkgs; config=config; };
   #"deconz" = import ./deconz/container.nix { pkgs = pkgs; config=config; stdenv=stdenv; };
   "rabbitmq" = import ./rabbitmq/container.nix { pkgs = pkgs; config=config; };
   #"woltest" = import ./woltest/container.nix { pkgs = pkgs; config=config; };
