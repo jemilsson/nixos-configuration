795964a977b948d134f1cf398c8887beebefde4b
Jonas Emilsson <jonas.emilsson@gmail.com>
2019-03-10 15:59:22 +0100

    freeswitch service

--- machines/brody/containers/pbx/configuration.nix
+++ machines/brody/containers/pbx/configuration.nix
@@ -25,4 +25,24 @@ environment.systemPackages = with pkgs; [
 
 services = {};
 
+systemd.services.freeswitch = {
+      enable = true;
+      description = "freeswitch";
+      after = [ "network.target" ];
+      wantedBy = [ "multi-user.target" ];
+      stopIfChanged = false;
+      serviceConfig = {
+        ExecStart = "${pkgs.unstable.freeswitch}/bin/freeswitch -conf /var/run/freeswitch/conf -log /var/log/freeswitch -db /var/db/freeswitch";
+        ExecReload = "${pkgs.coreutils}/bin/kill -HUP $MAINPID";
+        Restart = "always";
+        RestartSec = "10s";
+        StartLimitInterval = "1min";
+        #StateDirectory = "/var/lib/deconz";
+        User = "freeswitch";
+        #DeviceAllow = "char-ttyUSB rwm";
+        #DeviceAllow = "char-usb_device rwm";
+        #AmbientCapabilities="CAP_NET_BIND_SERVICE CAP_KILL CAP_SYS_BOOT CAP_SYS_TIME";
+      };
+    };
+
 }
