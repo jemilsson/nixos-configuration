58e5dfb79349ae92e200260e72581e77f876a5ad
Jonas Emilsson <jonas.emilsson@gmail.com>
2019-03-24 18:33:53 +0100

    no ipv4

--- machines/brody/containers/stubby/container.nix
+++ machines/brody/containers/stubby/container.nix
@@ -2,7 +2,7 @@
 {
   config = import ./configuration.nix { pkgs = pkgs; config=config; };
   hostBridge = "br1020";
-  localAddress = "10.5.20.12/24";
+  #localAddress = "10.5.20.12/24";
   localAddress6 = "2001:470:dc6b::12/64";
   autoStart = true;
   privateNetwork = true;
