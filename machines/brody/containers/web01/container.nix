28c09403ad704e8405fe7cda5042f89fac61b472
Jonas Emilsson <jonas.emilsson@gmail.com>
2019-03-29 14:45:18 +0100

    web

--- machines/brody/containers/web01/container.nix
+++ machines/brody/containers/web01/container.nix
@@ -3,6 +3,8 @@
   config = import ./configuration.nix { pkgs = pkgs; config=config; };
   hostBridge = "br1020";
   localAddress6 = "2001:470:dc6b::11/64";
+  localAddress = "10.5.20.11/24";
+
   autoStart = true;
   privateNetwork = true;
 }
