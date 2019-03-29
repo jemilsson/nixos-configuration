e32ca17e7f24215b24e90d5e572637c2f4fdbe1b
Jonas Emilsson <jonas.emilsson@gmail.com>
2019-03-29 20:01:42 +0100

    prometheus ipv6

--- machines/brody/containers/prm01/configuration.nix
+++ machines/brody/containers/prm01/configuration.nix
@@ -87,19 +87,8 @@ in
 {
   imports = [
     ../../../../config/minimum.nix
+    ../public_server.nix
 ];
-
-networking = {
-  firewall = {
-    enable = false;
-  };
-  nameservers = [ "10.5.20.1" ];
-
-  defaultGateway = {
-    address = "10.5.20.1";
-    interface = "eth0";
-  };
-};
 
 environment.systemPackages = with pkgs; [
 
