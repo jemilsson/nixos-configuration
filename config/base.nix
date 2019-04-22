5ecd1079e810427efb66568fef2ca1ddcdde87f7
Jonas Emilsson <jonas.emilsson@gmail.com>
2019-04-22 20:03:16 +0200

    emacs user service

--- config/base.nix
+++ config/base.nix
@@ -31,10 +31,10 @@ services = {
   mingetty = {
     helpLine = "test";
   };
-  #emacs = {
-  #  enable = true;
-  #  defaultEditor = true;
-  #};
+  emacs = {
+    enable = true;
+    defaultEditor = true;
+  };
 
 };
 
