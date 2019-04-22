19c262a0c4d90b0ddd04381cdadb7c5245fa0a98
Jonas Emilsson <jonas.emilsson@gmail.com>
2019-04-22 19:32:45 +0200

    nixos-unstable-small

--- add_channels.sh
+++ add_channels.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos-unstable
+sudo nix-channel --add https://nixos.org/channels/nixos-unstable-small nixos-unstable-small
 sudo nix-channel --add https://nixos.org/channels/nixos-19.03 nixos
 sudo nix-channel --update
