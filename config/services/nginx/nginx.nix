f89f6c8a2b2754d5b95a6bbf8a310c3d62653e88
Jonas Emilsson <jonas.emilsson@gmail.com>
2019-08-25 17:12:02 +0200

    improved sslCiphers

--- config/services/nginx/nginx.nix
+++ config/services/nginx/nginx.nix
@@ -6,7 +6,7 @@
        recommendedProxySettings=true;
        recommendedTlsSettings = true;
        recommendedGzipSettings = true;
-       sslCiphers = "AES256+EECDH:AES256+EDH:!aNULL";
+       sslCiphers = "EECDH+AESGCM:EDH+AESGCM";
        appendHttpConfig="server_names_hash_bucket_size 128; add_header Strict-Transport-Security \"max-age=31536000; includeSubDomains; preload\" always;";
        sslDhparam = "/var/lib/dhparams/nginx.pem";
        statusPage = true;
