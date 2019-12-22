baa6044951df78876d42452f500009cc656a6dcb
Jonas Emilsson <jonas.emilsson@gmail.com>
2019-12-22 21:57:18 +0100

    19.09

--- add_channels.sh
+++ add_channels.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 nix-channel --add https://nixos.org/channels/nixos-unstable nixos-unstable
 nix-channel --add https://nixos.org/channels/nixos-unstable-small nixos-unstable-small
-nix-channel --add https://nixos.org/channels/nixos-19.03 nixos
+nix-channel --add https://nixos.org/channels/nixos-19.09 nixos
 nix-channel --update
