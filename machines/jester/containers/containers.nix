4827151a88f9caf082108bcde89f36ef5c051b0a
Jonas Emilsson <jonas.emilsson@gmail.com>
2020-03-30 16:20:06 +0200

    client2

--- machines/jester/containers/containers.nix
+++ machines/jester/containers/containers.nix
@@ -1,5 +1,5 @@
 { config, pkgs, stdenv, ... }:
 {
   "testclient1" = import ./testclient1/container.nix { pkgs = pkgs; config=config; };
-  "testclient2" = import ./testclient1/container.nix { pkgs = pkgs; config=config; };
+  "testclient2" = import ./testclient2/container.nix { pkgs = pkgs; config=config; };
 }
