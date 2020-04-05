521d9acdd7461a8106a2dac27312a73b03291b16
Jonas Emilsson <jonas.emilsson@gmail.com>
2020-04-05 15:12:13 +0200

    cpu governor

--- config/laptop_base.nix
+++ config/laptop_base.nix
@@ -18,8 +18,8 @@
     tlp = {
       enable = true;
       extraConfig = ''
-      CPU_SCALING_GOVERNOR_ON_AC=ondemand
-      CPU_SCALING_GOVERNOR_ON_BAT=conservative
+      CPU_SCALING_GOVERNOR_ON_AC=performance
+      CPU_SCALING_GOVERNOR_ON_BAT=powersave
       '';
     };
   };
