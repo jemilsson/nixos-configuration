1e3c332a98bbfd6506e11fc8a998bbff3566c0ca
Jonas Emilsson <jonas.emilsson@gmail.com>
2020-04-21 13:21:53 +0200

    nginx

---
+++ machines/brody/containers/nginx/configuration.nix
@@ -0,0 +1,33 @@
+{ config, pkgs, ... }:
+let
+in
+{
+  imports = [
+    ../../../../config/services/nginx/nginx.nix
+    ../public_server.nix
+];
+environment.systemPackages = with pkgs; [
+];
+
+services = {
+  nginx.virtualHosts = {
+
+        "default.jonasem.com" = {
+          default = true;
+          extraConfig = "return 444;";
+        };
+
+       "he.jonasem.com" = {
+         forceSSL = true;
+         enableACME = true;
+         locations = {
+           "/" = {
+           root = "/var/www/he.jonasem.com";
+           index = "index.html";
+         };
+         };
+       };
+   };
+};
+
+}
