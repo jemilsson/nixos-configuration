b496cea52e110ee61d9dd703e314cf35ed122fdc
Jonas Emilsson <jonas.emilsson@gmail.com>
2020-04-21 21:49:13 +0200

    recommended cipherlist

--- config/services/nginx/nginx.nix
+++ config/services/nginx/nginx.nix
@@ -6,7 +6,6 @@
        recommendedProxySettings=true;
        recommendedTlsSettings = true;
        recommendedGzipSettings = true;
-       sslCiphers = "TLS_AES_256_GCM_SHA384:TLS_CHACHA20_POLY1305_SHA256";
        appendHttpConfig="server_names_hash_bucket_size 128; add_header Strict-Transport-Security \"max-age=31536000; includeSubDomains; preload\" always;";
        sslDhparam = "/var/lib/dhparams/nginx.pem";
        statusPage = true;
