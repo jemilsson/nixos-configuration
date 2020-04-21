6e3f6251bc4d250e8f3f535d91533e9714d4f8da
Jonas Emilsson <jonas.emilsson@gmail.com>
2020-04-21 13:20:27 +0200

    web01

--- machines/brody/containers/web01/container.nix
+++ machines/brody/containers/web01/container.nix
@@ -2,7 +2,7 @@
 {
   config = import ./configuration.nix { pkgs = pkgs; config=config; };
   hostBridge = "br1020";
-  localAddress6 = "2001:470:dc6b::11/64";
+  localAddress6 = "2a0e:b107:330::11/64";
   localAddress = "10.5.20.11/24";
 
   autoStart = true;
