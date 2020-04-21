e063517e3bc27709bcd7a5f1952606c5b1ec904a
Jonas Emilsson <jonas.emilsson@gmail.com>
2020-04-21 21:08:28 +0200

    stricter cipherlist

--- config/services/nginx/nginx.nix
+++ config/services/nginx/nginx.nix
@@ -6,7 +6,7 @@
        recommendedProxySettings=true;
        recommendedTlsSettings = true;
        recommendedGzipSettings = true;
-       sslCiphers = "EECDH+aRSA+AESGCM:EDH+aRSA:EECDH+aRSA:+AES256:+AES128:!SHA1:!SHA256:!SHA384:!CAMELLIA:!SEED:!3DES:!DES:!RC4:!eNULL";
+       sslCiphers = "EECDH+aRSA+AESGCM:EDH+aRSA:EECDH+aRSA:+AES256:!AES128:!SHA1:!SHA256:!SHA384:!CAMELLIA:!SEED:!3DES:!DES:!RC4:!eNULL";
        appendHttpConfig="server_names_hash_bucket_size 128; add_header Strict-Transport-Security \"max-age=31536000; includeSubDomains; preload\" always;";
        sslDhparam = "/var/lib/dhparams/nginx.pem";
        statusPage = true;
