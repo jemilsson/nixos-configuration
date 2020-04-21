e43928318c257acefd83dfb7165b4a284bbb3ee8
Jonas Emilsson <jonas.emilsson@gmail.com>
2020-04-21 20:32:35 +0200

    ip conflict

--- machines/brody/containers/rpki/container.nix
+++ machines/brody/containers/rpki/container.nix
@@ -2,7 +2,7 @@
 {
   config = import ./configuration.nix { pkgs = pkgs; config=config; };
   hostBridge = "br1020";
-  localAddress6 = "2a0e:b107:330::11/64";
+  localAddress6 = "2a0e:b107:330::16/64";
   autoStart = true;
   privateNetwork = true;
 }
