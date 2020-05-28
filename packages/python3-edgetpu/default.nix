7d0bac11f065b250bd5f985cc01d466f4a87a7f8
Jonas Emilsson <jonas.emilsson@gmail.com>
2020-05-28 09:23:09 +0200

    tflite

--- packages/python3-edgetpu/default.nix
+++ packages/python3-edgetpu/default.nix
@@ -1,12 +1,16 @@
 { stdenv, fetchurl, dpkg   }:
 #with import <nixpkgs> {};
+let
+  libedgetpu-dev = pkgs.callPackage ../libedgetpu-dev/default.nix {};
+  libedgetpu-max = pkgs.callPackage ../libedgetpu-max/default.nix {};
+in
 stdenv.mkDerivation{
   name = "libedgetpu-max_14";
   src = fetchurl {
     url = "https://packages.cloud.google.com/apt/pool/python3-edgetpu_14.0_amd64_1131406b35ed7ea4c478aa8953250bd271a0a4218ef15641b4bc8bc1becaae42.deb";
     sha256 = "1131406b35ed7ea4c478aa8953250bd271a0a4218ef15641b4bc8bc1becaae42";
   };
-  buildInputs = [ dpkg ];
+  buildInputs = [ dpkg libedgetpu-dev libedgetpu-max ];
 
   dontConfigure = true;
   dontBuild = true;
