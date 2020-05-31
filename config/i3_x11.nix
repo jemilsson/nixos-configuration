83d2c38e2eb3151c94efa4257d6c1eba72afd0f0
Jonas Emilsson <jonas.emilsson@gmail.com>
2020-05-31 12:56:41 +0200

    dev stuff

--- config/i3_x11.nix
+++ config/i3_x11.nix
@@ -1,6 +1,6 @@
 { config, lib, pkgs, ... }:
 let
-  taffybar = unstable.pkgs.haskellPackages.ghcWithPackages (self: [
+  taffybar = pkgs.unstable.haskellPackages.ghcWithPackages (self: [
     self.taffybar
     pkgs.hicolor-icon-theme
     pkgs.paper-icon-theme
