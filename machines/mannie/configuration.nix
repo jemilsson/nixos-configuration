ea30f85a8281b2f5bb18537fdd7063de0f46e2fc
Jonas Emilsson <jonas.emilsson@gmail.com>
2020-06-08 21:53:10 +0200

    bgpasd

--- machines/mannie/configuration.nix
+++ machines/mannie/configuration.nix
@@ -29,6 +29,7 @@ in
 
   boot.extraModulePackages = [ kernel.wireguard ];
   boot.kernel.sysctl."net.ipv4.ip_forward" = 1;
+  boot.kernel.sysctl."net.ipv6.conf.all.forwarding" = 1;
 
   networking = {
     hostName = "mannie";
