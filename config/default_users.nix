1e15c80841acae4c4ed88c14ebada2cad13470ee
Jonas Emilsson <11139713+jemilsson@users.noreply.github.com>
2020-06-24 08:15:33 +0200

    docker

--- config/default_users.nix
+++ config/default_users.nix
@@ -10,7 +10,7 @@ in
      group = "users";
      uid = 1000;
      isSystemUser = false;
-     extraGroups = [ "wheel" "networkmanager" "libvirtd" ];
+     extraGroups = [ "wheel" "networkmanager" "libvirtd" "docker" ];
      shell = "/run/current-system/sw/bin/zsh";
      openssh.authorizedKeys.keys = sshKeys.jonas;
   };
