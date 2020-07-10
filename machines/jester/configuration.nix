5fa830dfd42ab89fda57119aac66ad0c76f724cc
Jonas Emilsson <11139713+jemilsson@users.noreply.github.com>
2020-07-10 09:56:16 +0200

    opengl

--- machines/jester/configuration.nix
+++ machines/jester/configuration.nix
@@ -68,10 +68,17 @@ in
    '';
  };
 
- hardware.pulseaudio.extraConfig = ''
-  load-module module-alsa-sink   device=hw:0,0 channels=4
-  load-module module-alsa-source device=hw:0,6 channels=4
-'';
+ hardware = {
+  pulseaudio.extraConfig = ''
+    load-module module-alsa-sink   device=hw:0,0 channels=4
+    load-module module-alsa-source device=hw:0,6 channels=4
+  '';
+
+  opengl = {
+    extraPackages = with pkgs; [ vaapiIntel libvdpau-va-gl vaapiVdpau intel-ocl ];
+  };
+
+ };
 
  inherit containers;
 
