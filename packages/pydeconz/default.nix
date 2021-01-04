6b1d059fab33d4ca10798ad03d7b24d232f8553e
Jonas Emilsson <jemilsson@users.noreply.github.com>
2021-01-04 23:29:10 +0100

    hasssio

--- packages/pydeconz/default.nix
+++ packages/pydeconz/default.nix
@@ -4,11 +4,11 @@
 
 python38.pkgs.buildPythonPackage rec {
   pname = "pydeconz";
-  version = "47";
+  version = "77";
 
   src = python38.pkgs.fetchPypi {
     inherit pname version;
-    sha256 = "1d8drgy712sxxvisdaz53jgyiiidbhvnbrsvgci6jh9b6s6189wb";
+    sha256 = "1d8drgy712sxxvisdaz53jgyiiidbhvnbrsvgci6jh9b6s6188wb";
   };
 
   buildInputs = [ pkgs.unstable.python38Packages.aiohttp ];
