2e7f4f6af30e2ad6e0c4779375169600eae80e32
Jonas Emilsson <jemilsson@users.noreply.github.com>
2021-01-05 19:44:59 +0100

    hassio

--- machines/brody/containers/rabbitmq/container.nix
+++ machines/brody/containers/rabbitmq/container.nix
@@ -4,7 +4,7 @@
   localAddress = "10.5.30.2/32";
   localAddress6 = "2a0e:b107:330:beef::2/128";
   hostAddress6 = "fe80::1/128";
-  hostAddress = "10.5.30.1/32";
+  hostAddress = "10.5.30.1";
   autoStart = true;
   privateNetwork = true;
 }
