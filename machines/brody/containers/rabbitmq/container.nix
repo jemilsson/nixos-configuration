900b4b4bb5680d15ebe998b4293464eed0e4c16a
Jonas Emilsson <jemilsson@users.noreply.github.com>
2021-01-05 19:37:15 +0100

    hassio

--- machines/brody/containers/rabbitmq/container.nix
+++ machines/brody/containers/rabbitmq/container.nix
@@ -1,7 +1,6 @@
 { config, pkgs, ... }:
 {
   config = import ./configuration.nix { pkgs = pkgs; config=config; };
-  #hostBridge = "br1020";
   localAddress = "10.5.30.2/32";
   localAddress6 = "2a0e:b107:330:beef::2/128";
   hostAddress6 = "fe80::1";
