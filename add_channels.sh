1e5ee3e2b34f765827026d513143c44e24867772
Jonas Emilsson <jonas.emilsson@gmail.com>
2021-03-23 08:15:07 +0100

    steam small

--- add_channels.sh
+++ add_channels.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 nix-channel --add https://nixos.org/channels/nixos-20.09 nixos
+nix-channel --add https://nixos.org/channels/nixos-20.09-small nixos-small
 nix-channel --add https://nixos.org/channels/nixos-unstable nixos-unstable
 nix-channel --add https://nixos.org/channels/nixos-unstable-small nixos-unstable-small
 nix-channel --add https://github.com/NixOS/nixos-hardware/archive/master.tar.gz nixos-hardware
