af405778ebd6d0e6170d9d48fe4f883c31740484
Jonas Emilsson <jonas.emilsson@gmail.com>
2021-09-04 18:43:43 +0200

    postgresql

--- config/services/prometheus/node_exporter.nix
+++ config/services/prometheus/node_exporter.nix
@@ -1,26 +1,32 @@
 { config, lib, pkgs, ... }:
 {
   #networking.firewall.allowedTCPPorts = [ 9100 ];
-  services.prometheus.exporters.node = {
-    enable = true;
-    enabledCollectors = [
-      "conntrack"
-      "diskstats"
-      "entropy"
-      "filefd"
-      "filesystem"
-      "loadavg"
-      "mdadm"
-      "meminfo"
-      "netdev"
-      "netstat"
-      "stat"
-      "time"
-      "vmstat"
-      "systemd"
-      "logind"
-      "interrupts"
-      "ksmd"
-    ];
+  services.prometheus.exporters = {
+    node = {
+      enable = true;
+      enabledCollectors = [
+        "conntrack"
+        "diskstats"
+        "entropy"
+        "filefd"
+        "filesystem"
+        "loadavg"
+        "mdadm"
+        "meminfo"
+        "netdev"
+        "netstat"
+        "stat"
+        "time"
+        "vmstat"
+        "systemd"
+        "logind"
+        "interrupts"
+        "ksmd"
+      ];
+    };
+    systemd = {
+      enable = true;
+    };
   };
+
 }
