1a9d04b6c676b81d3663a14192751419cc4e9b75
Jonas Emilsson <jonas.emilsson@gmail.com>
2023-04-04 19:21:14 +0200

    add node nixos exporter

--- config/services/prometheus/node_exporter.nix
+++ config/services/prometheus/node_exporter.nix
@@ -24,9 +24,30 @@
         "ksmd"
       ];
     };
+
+    extraFlags = [
+      "--collector.textfile.directory /run/prometheus-node-exporter/"
+    ];
     systemd = {
       enable = true;
     };
   };
 
+  /*
+    config.system.nixos.version;
+    config.system.nixos.release;
+    config.system.nixos.codeName
+    config.system.configurationRevision
+  */
+
+  environment.etc.prometheus.textfile = {
+    nixosVersion.text = "${config.system.nixos.version}";
+    nixosRelease.text = "${config.system.nixos.release}";
+    nixosCodeName.text = "${config.system.nixos.codeName}";
+    nixosConfigurationRevision.text = "${config.system.nixos.configurationRevision}";
+  };
+
 }
+
+
+
