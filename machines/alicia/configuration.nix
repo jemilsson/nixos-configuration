e692461a1ef93760a6c2be0f590a611698a8816e
Jonas Emilsson <jonas.emilsson@gmail.com>
2024-04-14 14:56:39 +0200

    stable kernel on alicia

--- machines/alicia/configuration.nix
+++ machines/alicia/configuration.nix
@@ -12,7 +12,7 @@
   system.stateVersion = "22.05";
 
 
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  #boot.kernelPackages = pkgs.linuxPackages_latest;
 
   networking = {
     hostName = "alicia";
