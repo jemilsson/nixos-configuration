ddebfee97cf5590310768f1024c4460b99bda1eb
Jonas Emilsson <jonas.emilsson@gmail.com>
2024-10-08 13:12:08 +0200

    define a newer continue

--- packages/continue/default.nix
+++ packages/continue/default.nix
@@ -1,30 +1,31 @@
-{  buildVscodeMarketplaceExtension, stdenv, lib, autoPatchelfHook }:
-buildVscodeMarketplaceExtension {
-        mktplcRef =
-          let
-            sources = {
-              "x86_64-linux" = {
-                arch = "linux-x64";
-                hash = "sha256-Xu+Ya8xCDkIgtk0CZ2KJQtTLPd8fZPgJuzvFiTryUEc=";
-              };
-            };
-          in
-          {
-            name = "continue";
-            publisher = "Continue";
-            version = "0.9.2156";
-          }
-          // sources.${stdenv.system};
-        nativeBuildInputs = lib.optionals stdenv.hostPlatform.isLinux [ autoPatchelfHook ];
-        buildInputs = [ stdenv.cc.cc.lib ];
-        meta = {
-          description = "Open-source autopilot for software development - bring the power of ChatGPT to your IDE";
-          downloadPage = "https://marketplace.visualstudio.com/items?itemName=Continue.continue";
-          homepage = "https://github.com/continuedev/continue";
-          license = lib.licenses.asl20;
-          maintainers = [ lib.maintainers.raroh73 ];
-          platforms = [
-            "x86_64-linux"
-          ];
+{  pkgs, stdenv, lib, autoPatchelfHook }:
+
+pkgs.vscode-utils.buildVscodeMarketplaceExtension {
+  mktplcRef =
+    let
+      sources = {
+        "x86_64-linux" = {
+          arch = "linux-x64";
+          hash = "sha256-Xu+Ya8xCDkIgtk0CZ2KJQtTLPd8fZPgJuzvFiTryUEc=";
         };
       };
+    in
+    {
+      name = "continue";
+      publisher = "Continue";
+      version = "0.9.215";
+    }
+    // sources.${stdenv.system};
+  nativeBuildInputs = lib.optionals stdenv.hostPlatform.isLinux [ autoPatchelfHook ];
+  buildInputs = [ stdenv.cc.cc.lib ];
+  meta = {
+    description = "Open-source autopilot for software development - bring the power of ChatGPT to your IDE";
+    downloadPage = "https://marketplace.visualstudio.com/items?itemName=Continue.continue";
+    homepage = "https://github.com/continuedev/continue";
+    license = lib.licenses.asl20;
+    maintainers = [ lib.maintainers.raroh73 ];
+    platforms = [
+      "x86_64-linux"
+    ];
+  };
+}
